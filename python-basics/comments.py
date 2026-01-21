5483ad79be8e4f78e3682d1d2ce27b042514f541
Maximilianus1 <mef56656@gmail.com>
2026-01-21 12:43:49 +0500

    Two files have been added, comments and variables! Enjoy your day!

---
+++ python-basics/comments.py
@@ -0,0 +1,10 @@
+#U: This is a comment
+#Me: Thank you!
+print("Hello, World!")
+
+print("Hello, World!") #U: This is a comment
+#Me: obviously
+
+#U: print("Hello, World!")
+#Me: No!
+print("Good Idea, Mate!")
---
+++ python-basics/variables.py
@@ -0,0 +1,11 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+globx = "awesome"
+
+def myfunc():
+  print("Python is " + globx)
+
+myfunc()
