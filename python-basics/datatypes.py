0c10bcbeb5cb52a4ddf181d2c4ce631cc1ed6efa
Maximilianus1 <mef56656@gmail.com>
2026-01-21 12:47:42 +0500

    datatypes is added. Did you know that python has complex and frozenset data types?

---
+++ python-basics/datatypes.py
@@ -0,0 +1,15 @@
+x = complex(5j)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+y = frozenset(("apple", "bluberry", "cherry"))
+
+#display x:
+print(y)
+
+#display the data type of x:
+print(type(y)) 
