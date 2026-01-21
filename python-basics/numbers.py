799265c925a17792ab2c83b046008b2bf903ff9f
Maximilianus1 <mef56656@gmail.com>
2026-01-21 12:53:14 +0500

    numbers.py and strings.py are added! What a happy pair!

---
+++ python-basics/numbers.py
@@ -0,0 +1,12 @@
+x = 32
+y = 11E4
+z = -47.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+print(x)
+print(y)
+print(z)
---
+++ python-basics/strings.py
@@ -0,0 +1,27 @@
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+yufheiuHfewusgfwugfewgfpuw
+wejuyfeiugerhyugfhwewrr
+w
+rswr
+w
+r
+ew
+r
+w
+rw
+r
+w
+r
+
+"""
+print(a)
+
+b = "Hello, World!"
+print(b.lower())
+
+age = 233
+txt = f"My name is Dracula, I am {age}"
+print(txt)
