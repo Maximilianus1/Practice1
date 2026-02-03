6fd31f3aed73bc3a013d4fa0701faa4e6e0c8a4c
Maximilianus1 <mef56656@gmail.com>
2026-02-04 01:09:27 +0500

    for_loop.py was added and modified

---
+++ Practice2/loops/for_loop.py
@@ -0,0 +1,28 @@
+fruits = ["apple", "banana", "cherry", "orange", "mango"]
+for x in fruits:
+  print(x)
+for x in "bananasango":
+  print(x)
+for x in range(16):
+  print(x)
+
+for x in range(12, 60):
+  print(x)
+
+for x in range(2, 40, 2):
+  print(x)
+
+for x in range(12):
+  print(x)
+else:
+  print("Finally finished!")
+
+adj = ["red", "big", "tasty", "juicy"]
+fruits = ["apple", "banana", "cherry", "pear"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+for x in [0, 1, 2]:
+  pass
